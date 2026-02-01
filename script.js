1d0557b90a6a1ddbc3ae599ba99f326ee7f19081
kryzko <66265498+kryzko@users.noreply.github.com>
2026-02-02 01:39:25 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 import Paho from "paho-mqtt";
 
-// Используйте библиотеку Paho MQTT
 const clientId = "client_Krushka" ;
 // Create a client instance: Broker, Port, Websocket Path, Client ID
 var client = new Paho.Client("c331dfc8368c4832b294451e6869daee.s1.eu.hivemq.cloud", Number(8883), "web_clientkrushka");
